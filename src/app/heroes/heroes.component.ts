95b91b5378dae37902ffb782e174802e917338d5
lizziehosler <lizzie.hosler@gmail.com>
2020-09-13 14:40:40 -0500

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { HEROES } from './../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  selectedHero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
